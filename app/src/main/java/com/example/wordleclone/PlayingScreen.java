3789a066a3cb89198c8117718c24e010779b5869
Azzazy768 <omar.azzazy20102005@gmail.com>
2025-08-21 22:00:32 +0300

    Added Buttons to go from screen to screen

--- app/src/main/java/com/example/wordleclone/PlayingScreen.java
+++ app/src/main/java/com/example/wordleclone/PlayingScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +12,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class PlayingScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_playing_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button PlayButton = findViewById(R.id.play_button);
+        ImageButton HomeButton = findViewById(R.id.Home_Button);
 
-        PlayButton.setOnClickListener(new View.OnClickListener() {
+        HomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,PlayingScreen.class);
+                Intent intent = new Intent(PlayingScreen.this,MainActivity.class);
                 startActivity(intent);
             }
         });
-
 
     }
 }
